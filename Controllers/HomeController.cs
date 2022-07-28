83636390862b0cf3b3cb3a9becf0fbf904bf637a
Dev-Annie <85464723+Dev-Annie@users.noreply.github.com>
2022-07-28 12:29:51 -0400

    index linked to itinerary, edit but not saving, title linked to home

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -37,12 +37,13 @@ public class HomeController : Controller
     {
         return View();
     }
-    public IActionResult Cart()
+
+    public IActionResult About()
     {
         return View();
     }
 
-    
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
